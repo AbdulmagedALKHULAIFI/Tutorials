b0d614b57a806a7f67f61576ca67270c9ef91b8a
AbdulmagedALKHULAIFI <43535031+AbdulmagedALKHULAIFI@users.noreply.github.com>
2021-09-22 23:09:47 +0200

    Add files via upload
    Post request with input file via powershell

---
+++ upload_file.ps1
@@ -0,0 +1,30 @@
+﻿Add-Type -AssemblyName System.Net.Http
+
+try{
+    $multipartContent = [System.Net.Http.MultipartFormDataContent]::new()
+
+    $FilePath = 'C:\Users\princ\Downloads\file.txt';
+    $URL = 'https://localhost:44363/WeatherForecast';
+
+    $fileBytes = [System.IO.File]::ReadAllBytes($FilePath);
+    $fileEnc = [System.Text.Encoding]::GetEncoding('UTF-8').GetString($fileBytes);
+    $boundary = [System.Guid]::NewGuid().ToString(); 
+    $LF = "`r`n";
+
+    $bodyLines = ( 
+        "--$boundary",
+        "Content-Disposition: form-data; name=`"file`"; filename=`"temp.txt`"",
+        "Content-Type: application/octet-stream$LF",
+        $fileEnc,
+        "--$boundary--$LF" 
+    ) -join $LF
+
+    Invoke-RestMethod -Uri $URL -Method Post -ContentType "multipart/form-data; boundary=`"$boundary`"" -Body $bodyLines
+    
+    echo "fin"
+} 
+catch{}
+
+finally{
+    
+}
